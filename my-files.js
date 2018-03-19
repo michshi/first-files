447ffbd9ad92e636ec87e760f246682b34ad196c
shieldsy123 <michael.t.shields@gmail.com>
2018-03-19 14:06:45 -0600

    commit

--- my-files.js
+++ my-files.js
@@ -1,6 +1,16 @@
 const { red, yellow } = require('chalk')
 const path = require('path')
+const fs = require('fs')
 
-const filename = path.join(__dirname, 'test.txt')
+// const filename = path.join(__dirname, 'text.txt')
+// console.log(yellow(filename))
 
-console.log(yellow(filename))
+fs.readdir(__dirname, (err, files) => {
+  if (err) {
+    console.log(red(err))
+  }
+  else {
+    console.log(files)
+  }
+})
+console.log(yellow('Hello'))
