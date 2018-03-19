adf817ceaedf3f4f98678adff67156d8a761f14d
shieldsy123 <michael.t.shields@gmail.com>
2018-03-19 14:58:03 -0600

    adds fruit to things.csv

--- my-files.js
+++ my-files.js
@@ -1,8 +1,8 @@
-const { red, yellow } = require('chalk')
+const { red, yellow, cyan } = require('chalk')
 const path = require('path')
 const fs = require('fs')
 
-const filename = path.join(__dirname, 'text.txt')
+const filename = path.join(__dirname, 'test.txt')
 
 // console.log(yellow(filename))
 
@@ -14,14 +14,65 @@ const filename = path.join(__dirname, 'text.txt')
 //     console.log(files)
 //   }
 // })
-// console.log(yellow('Hello'))
+// console.log(yellow('Hello!'))
 
-fs.readFile(filename, 'utf-8', (err, data) => {
-  if (err) {
-    return console.log(err)
-  }
+// fs.readFile(filename, 'utf8', (err, data) => {
+//   if (err) {
+//     return console.log(err)
+//   }
+//
+//   const lines = data
+//     .split('\n')
+//     .filter(x => x.length > 0)
+//     .map((el, idx) => `${idx}: ${el}`)
+//
+//   console.log(lines)
+// })
 
-  const lines = data.split('\n').filter(x => x.length > 0)
+// try {
+//   const syncContents = fs.readFileSync('klsjdflksdjflkdsjfldksfj', 'utf8')
+//   console.log(cyan(syncContents))
+// }
+// catch (err) {
+//   console.log(red(err))
+// }
 
-  console.log(lines)
-})
+// fs.unlink(filename, (err) => {
+//   if (err) {
+//     console.log(red(err))
+//   }
+//   else {
+//     console.log('file has been anhilated')
+//   }
+// })
+
+function addFruit(type, count, filename='things.csv') {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.log(red(err))
+    }
+    else {
+      const lines = data
+        .split('\n')
+        .filter(x => x.length > 0)
+        .map(x => x.split(','))
+
+      const fruits = {}
+      lines.forEach(x => fruits[x[0]] = x[1])
+      fruits[type] = count
+
+      const newLines = []
+      Object.keys(fruits).forEach(key => {
+        newLines.push(`${key},${fruits[key]}`)
+      })
+
+      const writeMe = newLines.join('\n')
+      fs.writeFile(filename, writeMe, 'utf8', (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+  })
+}
+addFruit('Apple', 10)
