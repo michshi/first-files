eb4df97d7b6757dd6aed59ae7820194cac85426c
shieldsy123 <michael.t.shields@gmail.com>
2018-03-19 14:18:39 -0600

    reads a file

--- my-files.js
+++ my-files.js
@@ -2,15 +2,26 @@ const { red, yellow } = require('chalk')
 const path = require('path')
 const fs = require('fs')
 
-// const filename = path.join(__dirname, 'text.txt')
+const filename = path.join(__dirname, 'text.txt')
+
 // console.log(yellow(filename))
 
-fs.readdir(__dirname, (err, files) => {
+// fs.readdir(__dirname, (err, files) => {
+//   if (err) {
+//     console.log(red(err))
+//   }
+//   else {
+//     console.log(files)
+//   }
+// })
+// console.log(yellow('Hello'))
+
+fs.readFile(filename, 'utf-8', (err, data) => {
   if (err) {
-    console.log(red(err))
+    return console.log(err)
   }
-  else {
-    console.log(files)
-  }
+
+  const lines = data.split('\n').filter(x => x.length > 0)
+
+  console.log(lines)
 })
-console.log(yellow('Hello'))
